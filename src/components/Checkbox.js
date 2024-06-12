0cb2b9cc1a6c668ded3e3595fd7956fd3d8f51af
INTESARSHAWON <intesarshawon@gmail.com>
2024-06-13 02:53:39 +0600

    Signup page

---
+++ src/components/Checkbox.js
@@ -0,0 +1,13 @@
+
+
+function Checkbox({text, ...rest}) {
+    return(
+        <label>
+              <input type="checkbox" {...rest} />
+              <span>{text}</span>
+        </label>
+    );
+    
+}
+
+export default Checkbox
---
+++ src/components/Illustration.js
@@ -0,0 +1,12 @@
+import classes from '../styles/Illustration.module.css';
+import signupImage from '../assets/images/signup.svg';
+
+function Illustration() {
+    return(
+        <div className={classes.illustration}>
+            <img src={signupImage} alt="Signup" />
+        </div>
+    );
+}
+
+export default Illustration
